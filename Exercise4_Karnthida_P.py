bfe6f3f20adbde9a7e0655378d7052d7fa2638f1
Karnthida <56229165+Karnthida@users.noreply.github.com>
2019-10-07 10:22:46 +0700

    Create Exercise4_Karnthida_P.py

---
+++ Exercise4_Karnthida_P.py
@@ -0,0 +1,9 @@
+FE = 85.00
+GB = 90.00
+ICS = 95.00
+CP = 95.00
+print("--- Your Score ---")
+print("Foundation English : ", FE)
+print("General Business : ", GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ", CP)
